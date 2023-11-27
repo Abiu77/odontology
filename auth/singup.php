8763c265e1918c71313da4fd7dbaabc8ea82012f
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2023-11-26 20:18:19 -0500

    Fix login and signup functionality

--- auth/singup.php
+++ auth/singup.php
@@ -11,25 +11,46 @@
   // Genera un hash seguro de la contraseña
   $hashedPassword = password_hash($password, PASSWORD_DEFAULT); 
 
-  $query = "INSERT INTO erpo_usersistema (nombre, apellido, username, correo, contrasenia, fingerprint)
-  VALUES ('$name', '$lastname', '$username', '$email', '$hashedPassword', '')";
+  // SQL INJECTION:
+  // Preparar la consulta con una consulta preparada
+  $query = "INSERT INTO erpo_usersistema (nombre, apellido, username, correo, contrasenia, fingerprint) VALUES (?, ?, ?, ?, ?, '')";
+  $stmt = mysqli_prepare($connect, $query);
 
-  $execute = mysqli_query($connect, $query);
+  // Verificar si la preparación fue exitosa
+  if ($stmt) {
+    // Vincular los parámetros
+    mysqli_stmt_bind_param($stmt, "sssss", $name, $lastname, $username, $email, $hashedPassword);
 
-  if($execute){
+    // Ejecutar la consulta
+    $execute = mysqli_stmt_execute($stmt);
+
+    // Cerrar la consulta preparada
+    mysqli_stmt_close($stmt);
+  } else {
+    // Manejar el error en la preparación de la consulta
+    die("Error en la preparación de la consulta: " . mysqli_error($connect));
+  }
+ 
+  if ($execute) {
+    // XSS:
+    // Utilizar htmlspecialchars para evitar ataques XSS
+    $message = htmlspecialchars("Se ha registrado correctamente");
     echo '
       <script>
-        alert("Usuario almacenado exitosamente");
+        alert("' . htmlspecialchars($message, ENT_QUOTES, 'UTF-8') . '");
         window.location = "../index.html";
       </script>
     ';
-  }else{
+    exit;
+  } else {
+    $message = htmlspecialchars("Usuario no almacenado, por favor verifique los datos introducidos");
     echo '
       <script>
-        alert("Intentalo de nuevo, usuario no almacenado");
+        alert("' . htmlspecialchars($message, ENT_QUOTES, 'UTF-8') . '");
         window.location = "../index.html";
       </script>
     ';
+    exit;
   }
 
   mysqli_close($connect);
