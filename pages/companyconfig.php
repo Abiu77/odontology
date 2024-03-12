31b733360b3c64202b82625cef2fd0a13159464a
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-03-12 16:28:45 -0500

    add: title

--- pages/companyconfig.php
+++ pages/companyconfig.php
@@ -7,7 +7,7 @@ include '../config/db.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>Configuración de la empresa</title>
   <link rel="stylesheet" href="../css/style.css">
   <link rel="stylesheet" href="../css/font.css">
   <link rel="stylesheet" href="../css/index.css">
