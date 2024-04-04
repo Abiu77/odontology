5e0aefebdc5f0784775b898218fb901697ac8bfd
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-04-03 19:45:48 -0500

    fix: identation

--- auth/companyconfig.php
+++ auth/companyconfig.php
@@ -1,26 +1,25 @@
 <?php
- include '../config/db.php'; 
+include '../config/db.php'; 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $document_type = $_POST['document_type'];
-    $business = $_POST['business'];
-    $address = $_POST['address'];
-    $phone = $_POST['phone'];
-    $email = $_POST['email'];
-   /*  $logo_temp = $_FILES['logo']['tmp_name'];
-    $logo_path = "ruta_de_guardado_en_servidor/" . $_FILES['logo']['name'];
-    move_uploaded_file($logo_temp, $logo_path); */
+  $document_type = $_POST['document_type'];
+  $business = $_POST['business'];
+  $address = $_POST['address'];
+  $phone = $_POST['phone'];
+  $email = $_POST['email'];
+  $logo = $_POST['logo'];
 
-    $query = "INSERT INTO erpo_empresa (id_tipodocidentidad, cmp_razonsocial, cmp_direccion, cmp_telefono, cmp_email) VALUES (?, ?, ?, ?, ?)";
-    $statement = $connect->prepare($query);
-    $statement->bind_param("sssss", $document_type, $business, $address, $phone, $email);
+  $query = "INSERT INTO erpo_empresa (id_tipodocidentidad, cmp_razonsocial, cmp_direccion, cmp_telefono, cmp_email, cmp_logo) VALUES (?, ?, ?, ?, ?, ?)";
+  $statement = $connect->prepare($query);
+  $statement->bind_param("ssssss", $document_type, $business, $address, $phone, $email, $logo);
 
-    if ($statement->execute()) {
-        echo "Los datos se han insertado correctamente.";
-        header("Location: ../pages/employeeconfig.php");
-        exit();
-    } else {
-        echo "Error al insertar datos: " . $connect->error;
-    }
+  if ($statement->execute()) {
+    echo "Los datos se han insertado correctamente.";
+    header("Location: ../pages/employeeconfig.php");
+    exit();
+  } else {
+    echo "Error al insertar datos: " . $connect->error;
+  }
 }
 ?>
+ 
