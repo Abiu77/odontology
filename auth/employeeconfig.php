0f586fcd125c07517e656c9c7b9f2d84ae0cd463
Humberto Yupanqui Apaza <yupanquidev@gmail.com>
2024-04-03 19:47:50 -0500

    fix: identation

--- auth/employeeconfig.php
+++ auth/employeeconfig.php
@@ -1,14 +1,31 @@
 <?php
-include '../config/db.php';
+include '../config/db.php'; 
 
-// Recibe datos del formulario
-$email = $_POST['correo'];
-$password = $_POST['contrasenia'];
+// Verifica si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Obtiene los datos del formulario
+  $country = $_POST['country'];
+  $province = $_POST['province'];
+  $tipo_documento = $_POST['select'];
+  $razon_social = $_POST['business'];
+  $direccion = $_POST['address'];
+  $telefono = $_POST['phone'];
+  $email = $_POST['email'];
+  $logo = $_POST['logo']
 
+  // Prepara y ejecuta la consulta SQL para insertar los datos en la base de datos
+  $query = "INSERT INTO erpo_empresa (country, province, id_tipodocidentidad, cmp_razonsocial, cmp_direccion, cmp_telefono, cmp_email, cmp_logo ) VALUES (?, ?, ?, ?, ?, ?)";
+  $statement = $connect->prepare($query);
+  $statement->bind_param("ssssss",   $country,  $province, $tipo_documento, $razon_social, $direccion, $telefono, $email, $logo);
 
-// SQL INJECTION:
-// Preparar la consulta con una consulta preparada
-$query = "SELECT * FROM erpo_usersistema WHERE correo=?";
-$stmt = mysqli_prepare($connect, $query);
-
+  if ($statement->execute()) {
+    // Si la consulta se ejecuta correctamente, redirige o muestra un mensaje de éxito
+    echo "Los datos se han insertado correctamente.";
+    // Puedes redirigir a otra página después de insertar los datos si lo deseas
+    header("Location: ../pages/employeeconfig.php");
+    exit();
+  } else {
+    echo "Error al insertar datos: " . $connect->error;
+  }
+}
 ?>
